19482960c7266ea2533dd872f4760a3c0f775f44
Aidan Tallon <aidantallon@hotmail.com>
2017-08-15 15:49:05 +0100

    add crouching

--- actors/square.rb
+++ actors/square.rb
@@ -8,21 +8,37 @@ class Square
     @height = height
     @jump_height = 100
     @jumpsquat_length = 10
-    @state = :grounded
+    @state = :standing
     @ground_speed = 2
     @air_speed = 1
 
     @initial_vertical_velocity = 20
     @min_vertical_velocity = -20
-    @current_vertical_velocity = 20
+    @current_vertical_velocity = 0
   end
 
   def draw
     if @state == :jumpsquat
       jumpsquat_draw
-    else
-      Gosu::draw_rect(@x, $window.height - @y - @height, @width, @height, Gosu::Color::WHITE)
+    elsif @state == :crouching
+      crouching_draw
+    elsif @state == :standing
+      standing_draw
+    elsif @state == :jumping
+      jumping_draw
     end
+  end
+
+  def crouching_draw
+    Gosu::draw_rect(@x, $window.height - @y - (@height / 2), @width, @height / 2, Gosu::Color::WHITE)
+  end
+
+  def standing_draw
+    Gosu::draw_rect(@x, $window.height - @y - @height, @width, @height, Gosu::Color::WHITE)
+  end
+
+  def jumping_draw
+    Gosu::draw_rect(@x, $window.height - @y - @height, @width, @height, Gosu::Color::WHITE)
   end
 
   def jumpsquat_draw
@@ -37,11 +53,20 @@ class Square
     @x += dist
   end
 
+  def start_jumpsquat
+      @state = :jumpsquat
+      @active_jumpsquat = @jumpsquat_length
+  end
+
+  def start_jump
+    @current_vertical_velocity = @initial_vertical_velocity
+    @state = :jumping
+  end
+
   def jumpsquat_action(controls)
     @active_jumpsquat -= 1
     if @active_jumpsquat <= 0
-      @current_vertical_velocity = @initial_vertical_velocity
-      @state = :jumping
+      start_jump
     end
   end
 
@@ -57,21 +82,33 @@ class Square
     @current_vertical_velocity = [@current_vertical_velocity - 1, @min_vertical_velocity].max
     if @y <= 0
       @y = 0
-      @state = :grounded
+      @state = :standing
     end
   end
 
-  def grounded_action(controls)
+  def standing_action(controls)
     b_down = controls.buttons_down
     if b_down.include? controls.up
-      @state = :jumpsquat
-      @active_jumpsquat = @jumpsquat_length
+      start_jumpsquat
     end
     if b_down.include? controls.left
       move_left(@ground_speed)
     end
     if b_down.include? controls.right
       move_right(@ground_speed)
+    end
+    if b_down.include? controls.down
+      @state = :crouching
+    end
+  end
+
+  def crouching_action(controls)
+    b_down = controls.buttons_down
+    if !b_down.include? controls.down
+      @state = :standing
+    end
+    if b_down.include? controls.up
+      start_jumpsquat
     end
   end
 
@@ -80,8 +117,10 @@ class Square
       jumpsquat_action(controls)
     elsif @state == :jumping
       jumping_action(controls)
-    elsif @state == :grounded
-      grounded_action(controls)
+    elsif @state == :standing
+      standing_action(controls)
+    elsif @state == :crouching
+      crouching_action(controls)
     end
   end
 end
